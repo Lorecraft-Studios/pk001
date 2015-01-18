ed129a37b4a9705bc4f9e2b6f0184eda12e7bef5
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-18 08:55:49 -1000

    Added Romulus Remus and Aerus to mobs.js

---
+++ gamedata/mobs.js
@@ -0,0 +1,21 @@
+var mobs = {};
+
+mobs.trainer = {
+	short: 'The old trainer',
+	long: 'An old man weathered by war stands here ready to train his recruits.'
+}
+
+mobs.romulus = {
+	short: 'Romulus',
+	long: 'A brazen kid known as \'Romulus\' stands here looking for trouble.'
+};
+
+mobs.remus = {
+	short: 'Remus',
+	long: 'Remus the mischievious kid runs around causing trouble.'
+};
+
+mobs.aerus = {
+	short: 'Aerus',
+	long: 'Aerus the shy little girl looks around uneasily.'
+};
