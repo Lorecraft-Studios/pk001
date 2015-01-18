15b41bbd0a06dfdfe69b3947de7fd3658beafd61
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-17 21:49:04 -1000

    Add commented seperators

--- rooms/rooms.js
+++ rooms/rooms.js
@@ -1,5 +1,6 @@
 var rooms = {};
 
+//***********************************************************//
 rooms.a001 = {
 	title: 'A Cobblestoned Courtyard',
 	desc: 'A worn stone courtyard sits right outside of the militia outpost.  A faint militia banner wavers in the distant.  You see small groups of militia leisurely swinging their swords. Rush-strewn training dummies line the courtyard.',
@@ -14,8 +15,9 @@ rooms.a001 = {
 	mobs: [],
 	items: []
 };
+//***********************************************************//
 
-
+//***********************************************************//
 rooms.a002 = {
 	title: 'A Militia Outpost',
 	desc: 'The stone building serves as a gathering point for the local militia in this area. Serving the small villages throughout this province, this is where most of the recruiting takes place for the imperial army.',
@@ -30,7 +32,9 @@ rooms.a002 = {
 	mobs: [],
 	items: []
 };
+//***********************************************************//
 
+//***********************************************************//
 rooms.a003 = {
 	title: 'The Village Square',
 	desc: 'Villagers congregate and sit around the stone well that supplies the lifeline of this village.  The local military academy sits to the west. A hovel lined path to the north and the largest hovel in the village to the south.  A road to the east disappears outside of the village.',
@@ -45,4 +49,5 @@ rooms.a003 = {
 	mobs: [],
 	items: []
 };
+//***********************************************************//
 
