7b40686fc010ee28dd3b7d7b713570e9e1a58877
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-19 13:08:50 -1000

    Add engine.echo function to engine.js

--- client/engine/engine.js
+++ client/engine/engine.js
@@ -3,3 +3,11 @@
 engine = {};
 
 engine.test = console.log('testing engine.test Hello')
+
+//echoes a message to the player log. Defaults to player p001
+//if no parameter is passed to player.
+engine.echo = function(msg, player) {
+	if (!player) {player = "p001"};
+	Player.update( { _id: player },{ $push: {eventLog: msg} } );
+};
+
