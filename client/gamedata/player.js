a061467ad3fd797a84a322cbe51200a4872f0c1d
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-18 21:03:33 -1000

    Converted player.js to use client-only minimongo instance

--- client/gamedata/player.js
+++ client/gamedata/player.js
@@ -1,7 +1,11 @@
-player = {
+//Create a new local minimongo database on the client only
+Player = new Mongo.Collection();
+
+Player.insert({
+	_id: "p001",
 	userName: "",
 	roomAt: "",
 	inv:[],
 	eventLog: []
-};
+});
 
