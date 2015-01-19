b9bf88ee5aabe21e4dc6c2d5115ba8a551eeafb1
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-19 13:18:31 -1000

    Added engine.clearPlayerEventLog() to engine.js

--- client/engine/engine.js
+++ client/engine/engine.js
@@ -2,12 +2,19 @@
 
 engine = {};
 
-engine.test = console.log('testing engine.test Hello')
+engine.test = console.log('testing engine.test Hello');
 
 //echoes a message to the player log. Defaults to player p001
 //if no parameter is passed to player.
-engine.echo = function(msg, player) {
+engine.echoPlayerEventLog = function(msg, player) {
 	if (!player) {player = "p001"};
 	Player.update( { _id: player },{ $push: {eventLog: msg} } );
 };
 
+
+//clears the players eventLog. Default parameter for player is p001
+//if nothing passed through function.
+engine.clearPlayerEventLog = function(player) {
+	if (!player) {player = "p001"};
+	Player.update( {_id: player },{ $set: {eventLog: []} });
+};
