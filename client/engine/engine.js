8b27e2a2571f7b8fc83283526b98b5d40c91bd52
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-19 15:00:30 -1000

    Added comments on the engine.teleportPlayer function in engine.js

--- client/engine/engine.js
+++ client/engine/engine.js
@@ -23,10 +23,20 @@ engine.clearPlayerEventLog = function(player) {
 //Teleports player to location.  Pass the room _id to the roomTo parameter.
 //If no arguement is passed to playerId, it will default to "p001"
 engine.teleportPlayer = function(roomTo, playerId) {
+
+  //set default playerId to "p991" if no playerId parameter was passed through		
   if (!playerId) {playerId = "p001"};
+
+  //Set roomAt to the ID of the room the player is currently in.
   var roomAt = Player.findOne({_id: playerId},{'roomAt': 1}).roomAt;
+
+  //Remove the player ID from the room that player is currently in.
   Rooms.update({_id: roomAt},{$pull:{'mobs':playerId}});
+
+  //Push the player ID to the room that he will be teleported to.
   Rooms.update({_id: roomTo},{$push:{'mobs':playerId}});
+
+  //Update the players roomAt key with the new room he's in.
   Player.update({_id: playerId},{$set:{'roomAt': roomTo}});
 }
 
