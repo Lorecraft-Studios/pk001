2b796540f09d7edb228d7079064d0ce7b510bb99
Raymond Luong <raymond@Raymonds-MacBook-Air.local>
2015-01-19 14:50:07 -1000

    Add engine.teleportPlayer function to engine.js

--- client/engine/engine.js
+++ client/engine/engine.js
@@ -18,3 +18,30 @@ engine.clearPlayerEventLog = function(player) {
 	if (!player) {player = "p001"};
 	Player.update( {_id: player },{ $set: {eventLog: []} });
 };
+
+
+//Teleports player to location.  Pass the room _id to the roomTo parameter.
+//If no arguement is passed to playerId, it will default to "p001"
+engine.teleportPlayer = function(roomTo, playerId) {
+  if (!playerId) {playerId = "p001"};
+  var roomAt = Player.findOne({_id: playerId},{'roomAt': 1}).roomAt._id;
+  Rooms.update({_id: roomAt},{$pull:{'mobs':playerId}});
+  Rooms.update({_id: roomTo},{$push:{'mobs':playerId}});
+  Player.update({_id: playerId},{$set:{'roomAt': roomTo}});
+}
+//currently the above funtion ghosts and theres no way to check if the ID is already in that room, so 
+//sometimes it'll have the same ID's multiple times in the same room.
+
+
+//copy paste above function to create teleportItem and teleportMob
+
+
+
+
+
+
+
+
+
+
+
