3d90b2b99ea68ae934a730be2cc955b3c3b3db58
CaptainKevin <kevin.egami@gmail.com>
2015-01-29 23:59:36 -0800

    Add comments to questEngine

--- client/gamedata/questEngine.js
+++ client/gamedata/questEngine.js
@@ -3,6 +3,7 @@ questEngine = {};
 
 questEngine.s001 = {
 	s1: function() {
+		//Training Dummy enters room for quest
 		Rooms.update({_id: 'r001'}, {$push:{'mobs':'m005'}});
 	}
 };
@@ -10,6 +11,7 @@ questEngine.s001 = {
 
 questEngine.s003 = {
 	s1: function() {
+		//Romolus Remus and Aerus enter the room
 		engine.moveMob('m002', 'west');
 		engine.moveMob('m003', 'west');
 		engine.moveMob('m004', 'west');
@@ -22,13 +24,17 @@ questEngine.s004 = {
 		var currentRoom = Player.findOne({_id: 'p001'}).roomAt;
 		if (Dialogue.findOne({_id: 'm001'}).diaStatus[0] === 1) {
 			engine.echoPlayerEventLog('You gracefully slash the dummy, it falls to the ground in bits.');
+			//Removes Target dummy from the room
 			Rooms.update({_id: currentRoom}, {$pull: {mobs: currentMob}});
+			//Moves Sarge dialogue along
 			Dialogue.update({_id: 'm001'}, {$set: {diaStatus: [3]}});
 		}
 	}
 };
 
 questEngine.s005 = {
+	//var to track if player has attacked mob, to prevent player from attacking
+	//more than once to prevent spam
 	attacked: 'no',
 	s1: function() {
 		var currentMob = Session.get('clickId');
@@ -40,11 +46,14 @@ questEngine.s005 = {
 			}
 		}
 		questEngine.s005.attacked = 'yes';
+		//conditional to check if all three mobs have been attacked
 		questEngine.s008.s1();
 	}
 };
 
 questEngine.s006 = {
+	//var to track if player has attacked mob, to prevent player from attacking
+	//more than once to prevent spam
 	attacked: 'no',
 	s1: function() {
 		var currentMob = Session.get('clickId');
@@ -56,11 +65,14 @@ questEngine.s006 = {
 			}
 		}
 		questEngine.s006.attacked = 'yes';
+		//conditional to check if all three mobs have been attacked
 		questEngine.s008.s1();
 	}
 };
 
 questEngine.s007 = {
+	//var to track if player has attacked mob, to prevent player from attacking
+	//more than once to prevent spam
 	attacked: 'no',
 	s1: function() {
 		var currentMob = Session.get('clickId');
@@ -75,10 +87,14 @@ questEngine.s007 = {
 			}
 		}
 		questEngine.s007.attacked = 'yes';
+		//conditional to check if all three mobs have been attacked
 		questEngine.s008.s1();
 	}
 };
 
+//conditional to check if all three mobs have been attacked.
+//moves dialogue along if true
+//speak to sarge to continue
 questEngine.s008 = {
 	s1: function() {
 		if (questEngine.s005.attacked === 'yes' && questEngine.s006.attacked === 'yes' && questEngine.s007.attacked === 'yes') {
@@ -87,8 +103,10 @@ questEngine.s008 = {
 	}
 };
 
+//After completing quest, Romulus and Remus move east and threaten player
 questEngine.s009 = {
 	s1: function() {
+		//Update room to show east exit
 		Rooms.update({_id: 'r001'}, {$set: {exits: {east: 'r002'}}});
 		engine.echoPlayerEventLog('Remus whispers, \"We\'ll be waiting for you outside.\"')
 		engine.moveMob('m002', 'east');
